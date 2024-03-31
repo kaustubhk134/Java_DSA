3b534bddb6366f36489b3a0567dd280f5445f610
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-03-31 23:49:56 +0530

    while loops

---
+++ P14_while_loop.java
@@ -0,0 +1,9 @@
+public class P14_while_loop{
+    public static void main(String args[]){
+        int counter = 0;;
+        while(counter <= 100){
+            System.out.println("Hello World "+ counter);
+            counter ++;
+        }
+    }
+}
