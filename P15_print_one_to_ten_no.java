6d8266c9f6c26481762fb1031ff390a2d588cbdf
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-02 20:57:45 +0530

    1 to 10 using for loop

---
+++ P15_print_one_to_ten_no.java
@@ -0,0 +1,10 @@
+public class P15_print_one_to_ten_no {
+    public static void main(String args []){
+        int n = 1;
+        while (n <= 10){
+            System.out.print(n+" ");
+            n ++;
+        }
+    }
+    
+}
