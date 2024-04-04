a9d674ea960904d6f065c0b5fa12669463bfd57e
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-04 20:48:29 +0530

    print no 1 to n

---
+++ P16_print_no_one_to_n.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class P16_print_no_one_to_n {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the number : ");
+        int range = sc.nextInt();
+        int counter = 1;
+        while (counter <= range){
+            System.out.print(counter+" ");
+            counter ++;
+        }
+    }
+    
+}
