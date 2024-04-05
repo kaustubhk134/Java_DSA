ce19a31f22dbbed4c16b783a54820ee431c45854
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-05 22:54:38 +0530

    sum of natural no

---
+++ P17_sum_of_natural_no.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class P17_sum_of_natural_no {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the number : ");
+        int range = sc.nextInt();
+        int sum = 0;
+
+        int counter = 1;
+        while (counter <= range){
+            sum = sum + counter;
+            counter ++;
+        }
+        System.out.print(sum);
+    }
+    
+}
