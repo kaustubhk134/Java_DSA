7235cc59be0d3af14e42f6d1b8a941c3b3f3d6a1
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-06 23:36:35 +0530

    print star for loop

---
+++ P18_printl_star_pattern_forloop.java
@@ -0,0 +1,9 @@
+public class P18_printl_star_pattern_forloop {
+    public static void main(String args[]){
+
+        for (int i = 1; i <= 4; i++){
+            System.out.println("****");
+        }
+    }
+    
+}
