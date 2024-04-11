a2746d3a54b72b0ffeacf4ca9b416c1e032a5714
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-11 22:58:22 +0530

    using break statement

---
+++ P21_break_statement.java
@@ -0,0 +1,19 @@
+// keep entering numbers until user enters multiple of 10.
+import java.util.*;
+public class P21_break_statement {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        for(int i = 1; ; i ++){
+            System.out.println("Enter the number : ");
+            int n = sc.nextInt();
+            if (n % 10 == 0){
+                break;
+            }
+        }
+        System.out.println("I am out of the LOOP");
+
+
+    }
+    
+}
