f433f671058339b1aa08b87c27da7c8c22fa1ea6
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-13 23:33:33 +0530

    continue statement

---
+++ P22_continue_statement.java
@@ -0,0 +1,17 @@
+// keep entering numbers and print them but don't print the multiples of 10.
+import java.util.*;
+public class P22_continue_statement {
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        for (int i = 1 ; ; i++){
+            System.out.println("Enter the number : ");
+            int n = sc.nextInt();
+            if (n % 10 == 0){
+                continue;
+            }
+            System.out.println("Your number was : "+ n);
+        }
+    }
+    
+}
