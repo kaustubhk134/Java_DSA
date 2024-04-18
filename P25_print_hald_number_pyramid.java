1361bc017590b65126aa3383494e6f0a5c686cae
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-18 23:52:08 +0530

    half num pyramid pattern

---
+++ P25_print_hald_number_pyramid.java
@@ -0,0 +1,12 @@
+public class P25_print_hald_number_pyramid {
+    public static void main(String args[]){
+
+        int n = 4;
+        for (int lines = 1; lines <= n; lines++){
+            for (int num = 1; num <= lines; num++){
+                System.out.print(num);
+            }
+            System.out.println();
+        }
+    }
+}
