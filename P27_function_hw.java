62476c714014bad0a9975a82b420bcdcf6fa1630
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-21 15:02:58 +0530

    first function

---
+++ P27_function_hw.java
@@ -0,0 +1,9 @@
+public class P27_function_hw{
+    public static void helloWorld(){
+        System.out.println("Hello World");
+        return;
+    }
+    public static void main(String args[]){
+        helloWorld(); //this is function call
+    }
+}
