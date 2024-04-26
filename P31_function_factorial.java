a32acc52c88d500fdeb22a9042eb4d3acc36d636
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-04-27 01:25:54 +0530

    function of factorial

---
+++ P31_function_factorial.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class P31_function_factorial {
+
+    public static int factorial(int num){
+        int mul = 1;
+        for (int i = 1; i <= num; i++){
+            mul = mul * i;
+        }
+        return (mul);
+    }
+    public static void main(String args[]){
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Enter the Number : ");
+        int num = sc.nextInt();
+        int mulVal = factorial(num);
+
+        System.out.println("Factorial is : "+ mulVal);
+    }
+}
