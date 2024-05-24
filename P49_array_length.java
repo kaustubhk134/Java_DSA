baae17a9237abf4ce4422150383210a23fafc295
kaustubhk134 <154357336+kaustubhk134@users.noreply.github.com>
2024-05-25 00:03:42 +0530

    length of array

---
+++ P49_array_length.java
@@ -0,0 +1,8 @@
+public class P49_array_length {
+    public static void main(String args[]){
+        int marks[] = new int [1056];
+
+        System.out.println("The length of array :"+ marks.length);
+    }
+    
+}
